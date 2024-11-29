09591cb877cd51a7bd7ccdf4675c259ea0da88cb
kcghuser <kddccochrane@gmail.com>
2024-11-28 19:07:36 -0600

    Added home page and changed a few things in the design.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   namespace :admin do
-    resources :stocks
-    resources :products
-    resources :categories
+    resources :orders
+    resources :products do
+      resources :stocks
+    end
+      resources :categories
   end
   devise_for :admins
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -11,10 +13,6 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Render dynamic PWA files from app/views/pwa/*
-  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
   # Defines the root path route ("/")
   root "home#index"
 
@@ -22,5 +20,13 @@ Rails.application.routes.draw do
     root to: "admin#index", as: :admin_root
   end
 
+  resources :categories, only: [:show]
+  resources :products, only: [:show]
+
   get "admin" => "admin#index"
+  get "cart" => "carts#show"
+  post "checkout" => "checkouts#create"
+  get "success" => "checkouts#success"
+  get "cancel" => "checkouts#cancel"
+  post "webhooks" => "webhooks#stripe"
 end
