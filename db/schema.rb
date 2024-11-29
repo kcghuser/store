462071359a750663ac11ecd300b84ad3a3ac0812
kcghuser <kddccochrane@gmail.com>
2024-11-28 19:34:39 -0600

    Fixed website so home page shows and fixed backend admin page. Products also show up on home page with categories.

--- db/schema.rb
+++ db/schema.rb
@@ -67,6 +67,26 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_20_084825) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "order_products", force: :cascade do |t|
+    t.integer "product_id", null: false
+    t.integer "order_id", null: false
+    t.string "size"
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_order_products_on_order_id"
+    t.index ["product_id"], name: "index_order_products_on_product_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "customer_email"
+    t.boolean "fulfilled"
+    t.integer "total"
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -81,5 +101,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_20_084825) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "admin_stocks", "products"
+  add_foreign_key "order_products", "orders"
+  add_foreign_key "order_products", "products"
   add_foreign_key "products", "categories"
 end
