e386fb5a8cea4f38e7055db65a6f85318ee535a2
kcghuser <kddccochrane@gmail.com>
2024-12-04 03:04:16 -0600

    Fixed ability to view product page.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,5 +2,10 @@ class ProductsController < ApplicationController
   include Pagy::Backend
   def show
     @pagy, @product = Product.find(params[:id])
+    @product = Product.find_by(id: params[:id])
+    unless @product
+      redirect_to products_path, alert: "Product not found"
+    end
+    
   end
 end
