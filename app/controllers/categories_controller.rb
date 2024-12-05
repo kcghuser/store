8de25428d2894141164b28691a0937559204f8a1
kcghuser <kddccochrane@gmail.com>
2024-12-05 04:00:55 -0600

    Tried to integrate stripe but ended up only finding out how to integrate individual products but only specific product.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -12,6 +12,12 @@ class CategoriesController < ApplicationController
       search_term = "%#{params[:search].downcase}%"
       @products = @products.where("LOWER(name) LIKE ?", search_term)
     end
+    if params[:new].present? && params[:new] == "true"
+      @products = @products.where("created_at >= ?", 3.days.ago)
+    end
+    if params[:recently_updated].present? && params[:recently_updated] == "true"
+      @products = @products.where("updated_at >= ?", 3.days.ago)
+    end
     @products = @products.page(params[:page]).per(5)
   end
 end
