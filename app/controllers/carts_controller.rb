afad6b3618908d209498dcbd8c9ea9a1f68e1e3e
kcghuser <kddccochrane@gmail.com>
2024-12-04 21:46:23 -0600

    Made changes and added more categories on with the new code. Added Images to make it look more professional.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,29 +1,5 @@
 class CartsController < ApplicationController
   def show
-    @cart_items = session[:cart] || []
-  end
-
-  def add_item
-    product = Product.find(params[:product_id])
-    size = params[:size]
-
-    session[:cart] ||= []
-
-    session[:cart] << { product_id: product.id, size: size }
-
-    respond_to do |format|
-      format.html { redirect_to cart_path, notice: "#{product.name} has been added to your cart." }
-      format.json { render json: { message: "#{product.name} added to the cart." }, status: :ok }
-    end
-    def clear_cart
-      # Clear the session cart
-      session[:cart] = []
-      redirect_to cart_path, notice: "Your cart has been cleared."
-    end
-    def checkout
-      # For simplicity, just clear the cart and redirect to a success page
-      session[:cart] = []
-      redirect_to success_path, notice: "Thank you for your purchase!"
-    end
+  
   end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,5 @@
 class ProductsController < ApplicationController
-  include Pagy::Backend
   def show
-    @pagy, @product = Product.find(params[:id])
-    @product = Product.find_by(id: params[:id])
-    unless @product
-      redirect_to products_path, alert: "Product not found"
-    end
-    
+    @product = Product.find(params[:id])
   end
 end
